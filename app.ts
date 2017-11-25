cc21db033643910b59afbbf4621b82844cf4b73e
barbosathiago <thiago.barbosa@infouniron.com>
2017-11-25 02:51:41 -0400

    Exemplos 02 - Funções

--- app.ts
+++ app.ts
@@ -13,3 +13,23 @@ let favoriteDroid
 favoriteDroid = 'BB-8'
 favoriteDroid = 8
 console.log("My favorite droid is " + favoriteDroid)
+
+
+let isEnoughToBeatMF = function (parsecs: number) : boolean {
+  return parsecs < 12
+}
+// Usar aspas para utilizar a template string
+let distance = 11
+console.log(`Is ${distance} parsecs enough to beat Millenium Falcon? ${isEnoughToBeatMF(distance) ? 'YES': 'NO'}`)
+
+// Arrow function
+let call =  (name: string) => console.log(`Do you copy, ${name}`)
+call('R2')
+
+
+function inc(speed: number, inc: number = 1): number{
+  return speed + inc
+}
+
+console.log(`inc (5,1) = ${inc(5,1)}`)
+console.log(`inc (5) = ${inc(5)}`)
--- dist/app.js
+++ dist/app.js
@@ -9,3 +9,18 @@ var favoriteDroid;
 favoriteDroid = 'BB-8';
 favoriteDroid = 8;
 console.log("My favorite droid is " + favoriteDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+// Usar aspas para utilizar a template string
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat Millenium Falcon? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO'));
+// Arrow function
+var call = function (name) { return console.log("Do you copy, " + name); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5,1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
